7462a9351f80d1db66d67ab117857aa3734bc4de
emmanuel <chifez1@gmail.com>
2023-12-12 11:51:10 +0000

    fixed the carousel issue

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,3 +1,7 @@
+//I need to figure out why this doesn't work
+
+'use client';
+
 import { ReactNode, useEffect, useState } from 'react';
 
 interface Props {
@@ -5,19 +9,44 @@ interface Props {
   currentIndex: number;
 }
 const Carousel = ({ children, currentIndex }: Props) => {
-  const [translateSize, setTranslateSize] = useState('0');
-
-  const translate = `-translate-x-[${translateSize}]`;
+  const [translateSize, setTranslateSize] = useState('-translate-x-[0]');
 
   useEffect(() => {
-    setTranslateSize(`${currentIndex * 100}%`);
+    setTranslateSize(`-translate-x-[${currentIndex * 100}%]`);
     console.log(translateSize);
   }, [currentIndex]);
 
   return (
     <div className={`w-full flex overflow-x-hidden`}>
-      <div className={`${translate} w-full flex transition`}>{children}</div>
+      <div className={`${translateSize} w-full flex transition`}>
+        {children}
+      </div>
     </div>
   );
 };
 export default Carousel;
+
+// 'use client';
+
+// import { ReactNode } from 'react';
+
+// interface Props {
+//   children: ReactNode | ReactNode[];
+//   currentIndex: number;
+// }
+// const Carousel = ({ children, currentIndex }: Props) => {
+//   const TRANSLATE = {
+//     0: '-translate-x-[0]',
+//     1: '-translate-x-[100%]',
+//     2: '-translate-x-[200%]',
+//   };
+
+//   return (
+//     <div className={`w-full flex overflow-x-hidden`}>
+//       <div className={`${TRANSLATE[currentIndex]} w-full flex transition`}>
+//         {children}
+//       </div>
+//     </div>
+//   );
+// };
+// export default Carousel;
